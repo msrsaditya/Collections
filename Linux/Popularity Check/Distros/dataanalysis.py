04046bf90e103aed7d335cadf8d6b3e7e83a4d48
Shashank Aditya <msrsaditya@gmail.com>
2024-05-22 12:38:15 +0530

    Update dataanalysis.py

--- Linux/Popularity Check/Distros/dataanalysis.py
+++ Linux/Popularity Check/Distros/dataanalysis.py
@@ -4,25 +4,33 @@ import numpy as np
 # Data
 distributions = [
     "ChromeOS", "Arch Linux", "Ubuntu", "Tails", "Fedora", 
-    "Kali Linux", "Linux Mint", "Debian", "Manjaro Linux", "Red Hat",
-    "openSUSE", "Gentoo", "NixOS", "elementary OS", "SteamOS", 
-    "CentOS", "Qubes OS", "Void Linux", "EndeavourOS", "Kubuntu",
-    "Solus Project", "Whonix", "Asahi Linux", "Rocky Linux", "Zorin OS",
-    "MX Linux", "KDE neon", "Linux from Scratch", "Nobara Project", "Lubuntu",
-    "AlmaLinux", "Garuda Linux", "Slackware", "Parrot OS", "deepin",
-    "Alpine Linux", "Guix", "Bedrock Linux", "HoloISO", "ArcoLinux",
-    "Puppy Linux", "Clear Linux", "VanillaOS", "KISS Linux", "Pop!_OS",
-    "Peppermint OS", "antiX Linux", "Feren OS", "RedStar OS", "Tiny Core Linux",
-    "Oracle Linux", "KaOS"
+    "Kali Linux", "Linux Mint", "Debian", "Pop!_OS", "Manjaro Linux",
+    "Red Hat", "openSUSE", "Gentoo", "NixOS", "elementary OS", 
+    "SteamOS", "CentOS", "Qubes OS", "Void Linux", "EndeavourOS", 
+    "Kubuntu", "Solus Project", "Whonix", "Asahi Linux", "Rocky Linux",
+    "Zorin OS", "MX Linux", "KDE neon", "Linux from Scratch", 
+    "Nobara Project", "Lubuntu", "AlmaLinux", "Xubuntu", "Garuda Linux",
+    "Slackware", "Parrot OS", "deepin", "Alpine Linux", "Guix",
+    "CrunchBang", "Bedrock Linux", "HoloISO", "ArcoLinux", "Antergos",
+    "CrunchBangplusplus", "Puppy Linux", "Raspbian", "BlackArch Official",
+    "Clear Linux", "VanillaOS", "KISS Linux", "CachyOS", "Peppermint OS",
+    "Mageia", "Parabola", "antiX Linux", "Bodhi Linux", "Feren OS",
+    "Linux Lite", "Official Arch Labs Linux", "RedStar OS", "Tiny Core Linux",
+    "GeckoLinux", "Sabayon", "Q4OS", "RhinoLinux", "BackBox", "ArchBang",
+    "Oracle Linux", "OpenMandriva", "Crux Linux", "Exherbo", "Kinoite",
+    "Scientific Linux", "Venom Linux", "Anarchy Linux", "RLXOS Dev",
+    "KaOS", "Alt Linux"
 ]
 
 popularity = [
-    574563, 257395, 227597, 103558, 97561, 94931, 91955, 79747, 69810, 37632,
-    31655, 25850, 24437, 23114, 18691, 16573, 15027, 13422, 13030, 11962,
-    11233, 8829, 8778, 8220, 7655, 7539, 6491, 6404, 5819, 5562,
-    5393, 5276, 5133, 4325, 3501, 3381, 3316, 1891, 1851, 1542,
-    1387, 1031, 1026, 972, 713, 628, 562, 521, 355, 284,
-    148, 23
+    574558, 257468, 227640, 103580, 97604, 94980, 92018, 79775, 70207, 69815,
+    37646, 31677, 25855, 24463, 23115, 18698, 16571, 15037, 13426, 13038,
+    11966, 11234, 8834, 8784, 8222, 7662, 7541, 6489, 6406, 5830, 5561,
+    5395, 5391, 5276, 5134, 4328, 3502, 3390, 3317, 2046, 1891, 1853,
+    1540, 1491, 1487, 1390, 1215, 1092, 1031, 1028, 972, 642, 628,
+    612, 577, 563, 543, 521, 435, 392, 356, 285, 202, 201,
+    196, 165, 160, 156, 148, 147, 137, 102, 90, 58, 53,
+    40, 35, 23, 15
 ]
 
 # Bar Plot
